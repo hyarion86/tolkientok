133ba51d6e58b40859d0c4314c8de9f3ab0ce8a3
hyarion86 <63123146+hyarion86@users.noreply.github.com>
2025-02-08 15:55:28 -0800

    Update useWikiArticles.ts

--- frontend/src/hooks/useWikiArticles.ts
+++ frontend/src/hooks/useWikiArticles.ts
@@ -15,56 +15,45 @@ export function useWikiArticles() {
   const [articles, setArticles] = useState<WikiArticle[]>([]);
   const [loading, setLoading] = useState(false);
   const [buffer, setBuffer] = useState<WikiArticle[]>([]);
-  const { currentLanguage } = useLocalization();
+  const {currentLanguage} = useLocalization()
 
-  const fetchArticles = async () => {
+  const fetchArticles = async (forBuffer = false) => {
     if (loading) return;
     setLoading(true);
     try {
       const response = await fetch(
-        `${currentLanguage.api}?${new URLSearchParams({
-          action: "query",
-          format: "json",
-          generator: "random",
-          grnnamespace: "0",
-          prop: "extracts|pageimages|info",
-          inprop: "url",
-          grnlimit: "20",
-          exintro: "1",
-          exlimit: "max",
-          exsentences: "5",
-          explaintext: "1",
-          piprop: "thumbnail",
-          pithumbsize: "400",
-          origin: "*",
-        })}`
+        currentLanguage.api +
+          new URLSearchParams({
+            action: "query",
+            format: "json",
+            generator: "random",
+            grnnamespace: "0",
+            prop: "extracts|pageimages|info",
+            inprop: "url",
+            grnlimit: "20",
+            exintro: "1",
+            exlimit: "max",
+            exsentences: "5",
+            explaintext: "1",
+            piprop: "thumbnail",
+            pithumbsize: "400",
+            origin: "*",
+          })
       );
 
-      if (!response.ok) {
-        throw new Error(`Error fetching articles: ${response.statusText}`);
-      }
-
       const data = await response.json();
-
-      if (!data.query) {
-        throw new Error("No query in response");
-      }
-
       const newArticles = Object.values(data.query.pages)
-        .map((page: any): WikiArticle => ({
+        .map((page: any): WikiArticle  => ({
           title: page.title,
           extract: page.extract,
           pageid: page.pageid,
           thumbnail: page.thumbnail,
           url: page.canonicalurl,
         }))
-        .filter(
-          (article) =>
-            article.thumbnail &&
-            article.thumbnail.source &&
-            article.url &&
-            article.extract
-        );
+        .filter((article) => article.thumbnail
+                             && article.thumbnail.source
+                             && article.url
+                             && article.extract);
 
       await Promise.allSettled(
         newArticles
@@ -72,23 +61,27 @@ export function useWikiArticles() {
           .map((article) => preloadImage(article.thumbnail!.source))
       );
 
-      setBuffer((prev) => [...prev, ...newArticles]);
+      if (forBuffer) {
+        setBuffer(newArticles);
+      } else {
+        setArticles((prev) => [...prev, ...newArticles]);
+        fetchArticles(true);
+      }
     } catch (error) {
       console.error("Error fetching articles:", error);
-    } finally {
-      setLoading(false);
     }
+    setLoading(false);
   };
 
   const getMoreArticles = useCallback(() => {
-    if (loading) return; // Prevent fetching while already loading
     if (buffer.length > 0) {
       setArticles((prev) => [...prev, ...buffer]);
       setBuffer([]);
+      fetchArticles(true);
     } else {
-      fetchArticles();
+      fetchArticles(false);
     }
-  }, [buffer, loading]);
+  }, [buffer]);
 
   return { articles, loading, fetchArticles: getMoreArticles };
 }
