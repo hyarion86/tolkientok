cf45d238c3dce35791730b6a33f89bb20400ef31
hyarion86 <63123146+hyarion86@users.noreply.github.com>
2025-02-08 15:46:37 -0800

    Update useWikiArticles.ts

--- frontend/src/hooks/useWikiArticles.ts
+++ frontend/src/hooks/useWikiArticles.ts
@@ -15,45 +15,47 @@ export function useWikiArticles() {
   const [articles, setArticles] = useState<WikiArticle[]>([]);
   const [loading, setLoading] = useState(false);
   const [buffer, setBuffer] = useState<WikiArticle[]>([]);
-  const {currentLanguage} = useLocalization()
+  const { currentLanguage } = useLocalization();
 
-  const fetchArticles = async (forBuffer = false) => {
+  const fetchArticles = async () => {
     if (loading) return;
     setLoading(true);
     try {
       const response = await fetch(
-        currentLanguage.api +
-          new URLSearchParams({
-            action: "query",
-            format: "json",
-            generator: "random",
-            grnnamespace: "0",
-            prop: "extracts|pageimages|info",
-            inprop: "url",
-            grnlimit: "20",
-            exintro: "1",
-            exlimit: "max",
-            exsentences: "5",
-            explaintext: "1",
-            piprop: "thumbnail",
-            pithumbsize: "400",
-            origin: "*",
-          })
+        `${currentLanguage.api}?${new URLSearchParams({
+          action: "query",
+          format: "json",
+          generator: "random",
+          grnnamespace: "0",
+          prop: "extracts|pageimages|info",
+          inprop: "url",
+          grnlimit: "20",
+          exintro: "1",
+          exlimit: "max",
+          exsentences: "5",
+          explaintext: "1",
+          piprop: "thumbnail",
+          pithumbsize: "400",
+          origin: "*",
+        })}`
       );
 
       const data = await response.json();
       const newArticles = Object.values(data.query.pages)
-        .map((page: any): WikiArticle  => ({
+        .map((page: any): WikiArticle => ({
           title: page.title,
           extract: page.extract,
           pageid: page.pageid,
           thumbnail: page.thumbnail,
           url: page.canonicalurl,
         }))
-        .filter((article) => article.thumbnail
-                             && article.thumbnail.source
-                             && article.url
-                             && article.extract);
+        .filter(
+          (article) =>
+            article.thumbnail &&
+            article.thumbnail.source &&
+            article.url &&
+            article.extract
+        );
 
       await Promise.allSettled(
         newArticles
@@ -61,12 +63,7 @@ export function useWikiArticles() {
           .map((article) => preloadImage(article.thumbnail!.source))
       );
 
-      if (forBuffer) {
-        setBuffer(newArticles);
-      } else {
-        setArticles((prev) => [...prev, ...newArticles]);
-        fetchArticles(true);
-      }
+      setBuffer(newArticles);
     } catch (error) {
       console.error("Error fetching articles:", error);
     }
@@ -77,11 +74,9 @@ export function useWikiArticles() {
     if (buffer.length > 0) {
       setArticles((prev) => [...prev, ...buffer]);
       setBuffer([]);
-      fetchArticles(true);
     } else {
-      fetchArticles(false);
+      fetchArticles();
     }
   }, [buffer]);
 
   return { articles, loading, fetchArticles: getMoreArticles };
-}
